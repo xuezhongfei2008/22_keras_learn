8412c82f899b1cb75809adf327b5ff46b71c0be7
gongxuefei <1025500740@qq.com>
2019-03-12 11:43:34 +0800

    Signed-off-by: gongxuefei <1025500740@qq.com>

---
+++ keras_MLP.py
@@ -0,0 +1,27 @@
+### Multilayer Perceptron (MLP):
+
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.optimizers import SGD
+
+model = Sequential()
+# Dense(64) is a fully-connected layer with 64 hidden units.
+# in the first layer, you must specify the expected input data shape:
+# here, 20-dimensional vectors.
+model.add(Dense(64, input_dim=20, init='uniform'))
+model.add(Activation('tanh'))
+model.add(Dropout(0.5))
+model.add(Dense(64, init='uniform'))
+model.add(Activation('tanh'))
+model.add(Dropout(0.5))
+model.add(Dense(2, init='uniform'))
+model.add(Activation('softmax'))
+
+sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='mean_squared_error', optimizer=sgd)
+
+# model.fit(X_train, y_train, nb_epoch=20, batch_size=16)
+# score = model.evaluate(X_test, y_test, batch_size=16)
+
+from keras.utils import plot_model
+plot_model(model,to_file='model.png')
